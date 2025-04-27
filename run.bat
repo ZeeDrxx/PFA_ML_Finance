ff332cf697f25abeb099982dbfe18d84356e6ca7
zeedrxx <zeedrxx@gmail.com>
2025-04-27 23:04:01 +0100

    Version Alpha - Rework

--- run.bat
+++ run.bat
@@ -1,13 +1,23 @@
 @echo off
-title 🚀 Lancement Détection de Fraude - ML_Finance
+title 🚀 Initialisation et Lancement Détection de Fraude - ML_Finance
+
+:: Si .venv n'existe pas, créer l'environnement virtuel
+if not exist ".venv" (
+    echo 🔵 Création de l'environnement virtuel...
+    python -m venv .venv
+)
 
 :: Activer l'environnement virtuel
 call .venv\Scripts\activate
 
+:: Installer les dépendances depuis requirements.txt
+echo 🛠️ Installation des dépendances...
+pip install -r requirements.txt
+
 :: Lancer FastAPI (backend)
 start cmd /k "cd backend\app && uvicorn main:app --reload"
 
-:: Attendre quelques secondes que le serveur démarre
+:: Attendre quelques secondes que FastAPI démarre
 timeout /t 3 > nul
 
 :: Lancer Streamlit (frontend)
